346cacbd8f660944a96c4355a092a736ac5ca54b
kvnvde0000 <83350769+kvnvde0000@users.noreply.github.com>
2021-04-28 15:04:04 +0200

    Added the code and the first .EXE

---
+++ Uselessbot.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <windows.h>
+using namespace std;
+int main()
+{
+ int salut;
+ system("color 70");
+ cout << "BONJOUR"<< endl << "Tape un nombre au hasard!"<< endl << "Votre nombre :";
+ cin>>salut;
+ system("color CD");
+ system("color DC");
+ system("color E4");
+ system("color 4E");
+ system("color B9");
+ system("color 45");
+ system("color 24");
+ system("color 35");
+ system("color 53");
+ system("color 0A");
+ system("color A0");
+ if(salut>10)cout<<"Tu vas bien?"<<endl;
+ else cout<<"AH BAHAHAH A PLUS"<<endl;
+ system("color CD");
+ system("color DC");
+ system("color E4");
+ system("color 4E");
+ system("color B9");
+ system("color 45");
+ system("color 24");
+ system("color 35");
+ system("color 53");
+ system("color 0A");
+ system("color A0");
+ system("color CD");
+ system("color DC");
+ system("color E4");
+ system("color 4E");
+ system("color B9");
+ system("color 45");
+ system("color 24");
+ system("color 35");
+ system("color 53");
+ system("color 0A");
+ system("color A0");
+ system("color 07");
+ int oui;
+ system("color 0A");
+ std:: cout<<"10=NON - 11=OUI"<<std:: endl<< "Votre nombre :"; 
+ cin>>oui;
+ if(oui>10)cout<<"COOL"<<endl;
+ else cout<<"raconte moi!"<<endl;
+ system("PAUSE");
+}
+
+
